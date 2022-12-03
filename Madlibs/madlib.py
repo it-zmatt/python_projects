1b3bbf1ca8fce2bce9033c4f7448f03d72f3ae95
it-zmatt <mohamed.taous@univ-khenchela.dz>
2022-12-03 13:21:10 +0100

    First game Madlib

---
+++ Madlibs/madlib.py
@@ -0,0 +1,10 @@
+noun = input("enter a noun: ")
+verb = input("enter a verb: ")
+adjective = input("enter a adjective: ")
+name = input("enter a name: ")
+
+madlib = f"just a random {noun} to {verb} game, so far the weather is so good and I am doing a decent job \
+    I am listening to a good playlist Rn some RnB french Rap and algerian Rai what a {adjective} mix so far \
+    {name} is stronger than ever "
+
+print(madlib)
